8ed80968d0e8945ae26bb9de79f40353c304cd41
liwenlong <liwenlong@oneapm.com>
2020-03-24 23:22:37 +0800

    daily update

--- src/main/java/algorithm/leetcode/A46_Permutations.java
+++ src/main/java/algorithm/leetcode/A46_Permutations.java
@@ -3,17 +3,21 @@ package algorithm.leetcode;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @see interview.offer.CharPermutation
+ * decision tree,backtrack
+ */
 public class A46_Permutations {
     public List<List<Integer>> permute(int[] nums) {
 
         List<Integer> temp = new ArrayList<>();
         List<List<Integer>> result = new ArrayList<>();
-        backTrack(result,temp,nums);
+        backtrack(result,temp,nums);
 
         return result;
     }
 
-    private void backTrack(List<List<Integer>> result,List<Integer> temp,int[] nums){
+    private void backtrack(List<List<Integer>> result,List<Integer> temp,int[] nums){
         if (temp.size() == nums.length){
             result.add(new ArrayList<>(temp));
         }else {
@@ -22,35 +26,9 @@ public class A46_Permutations {
                     continue;
                 }
                 temp.add(nums[i]);
-                backTrack(result,temp,nums);
+                backtrack(result,temp,nums);
                 temp.remove(temp.size()-1);
             }
-        }
-    }
-
-    public static void main(String[] args) {
-        A46_Permutations p = new A46_Permutations();
-        int[] a = {1,2,3};
-        System.out.println(p.permuteDFS(a));
-    }
-
-    public List<List<Integer>> permuteDFS(int[] nums) {
-
-        List<List<Integer>> result = new ArrayList<>();
-        dfs(result,nums,0);
-
-        return result;
-    }
-
-    private void dfs(List<List<Integer>> result,int[] nums,int start){
-        //TODO
-    }
-
-    private void swap(int[] nums,int i,int j){
-        if (i!=j){
-            int t = nums[i];
-            nums[i] = nums[j];
-            nums[j] = t;
         }
     }
 }
