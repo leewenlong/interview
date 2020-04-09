705dd67432c88d0fec2c3fe5b7ef43047a60aa61
liwenlong <liwenlong@oneapm.com>
2020-04-09 23:08:39 +0800

    daily update

---
+++ src/main/java/algorithm/leetcode/A226_InvertBinaryTree.java
@@ -0,0 +1,15 @@
+package algorithm.leetcode;
+
+public class A226_InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode t = root.left;
+        root.left = root.right;
+        root.right = t;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
